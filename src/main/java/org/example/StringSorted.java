082800c584e9553f83d508159d5bc94d9d7dc859
Rinat <61909431+Rinatik13@users.noreply.github.com>
2022-09-28 13:29:53 +0500

    Initial commit

---
+++ src/main/java/org/example/StringSorted.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringSorted {
+    Map<Character, Integer> map = new HashMap<>();
+    public String startSorted(String text){
+        createMap();
+        String result = "";
+        String[] textMap = text.split(" ");
+
+        for (int y = 0; y < textMap.length-1; y++){
+            for (int i = y; i < textMap.length-1; i++) {
+                char charA = textMap[i].charAt(0);
+                char charB = textMap[i + 1].charAt(0);
+                int a = map.get(charA);
+                int b = map.get(charB);
+                if (a < b) {
+                    String to = textMap[i];
+                    textMap[i] = textMap[i + 1];
+                    textMap[i + 1] = to;
+                }
+            }
+            for (int i = textMap.length-1; i > 0; i--) {
+                char charA = textMap[i].charAt(0);
+                char charB = textMap[i - 1].charAt(0);
+                int a = map.get(charA);
+                int b = map.get(charB);
+                if (a > b) {
+                    String to = textMap[i];
+                    textMap[i] = textMap[i - 1];
+                    textMap[i - 1] = to;
+                }
+            }
+        }
+        for (int i = 0; i<textMap.length; i++){
+            result += textMap[i];
+            if (i == textMap.length-1){
+                break;
+            }
+            else {
+                result+= " ";
+            }
+        }
+        return result;
+    }
+
+
+    public void createMap (){
+        this.map.put('2',1);
+        this.map.put('3',2);
+        this.map.put('4',3);
+        this.map.put('5',4);
+        this.map.put('6',5);
+        this.map.put('7',6);
+        this.map.put('8',7);
+        this.map.put('9',8);
+        this.map.put('T',9);
+        this.map.put('J',10);
+        this.map.put('Q',11);
+        this.map.put('K',12);
+        this.map.put('A',13);
+    }
+}
---
+++ src/test/java/org/example/StringSortedTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringSortedTest extends StringSorted {
+
+    String text = "AS 2D JH TC 3S";
+    String text2 = "KS KH AH 2C TD";
+    String text3 = "7D TS JC AH AD";
+
+    @Test
+    public void testStart() {
+        startSorted(text);
+    }
+
+    @Test
+    public void testReturn(){
+        String result = null;
+        result = startSorted(text);
+        assertNotEquals(null,result);
+    }
+
+    @Test
+    public void testSorted(){
+        String exResult = "AS JH TC 3S 2D";
+        String result = startSorted(text);
+        assertEquals(exResult,result);
+    }
+
+    @Test
+    public void testSorted2(){
+        String exResult = "AH KS KH TD 2C";
+        String result = startSorted(text2);
+        assertEquals(exResult,result);
+    }
+
+    @Test
+    public void testSorted3(){
+        String exResult = "AH AD JC TS 7D";
+        String result = startSorted(text3);
+        assertEquals(exResult,result);
+    }
+
+}
